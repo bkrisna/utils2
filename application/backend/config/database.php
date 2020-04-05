e06eec132a29ae036033859c61de1baa5140a1a9
bkrisna <b.krisnamurti@gmail.com>
2020-04-05 14:21:26 +0700

    Update database.php

--- application/backend/config/database.php
+++ application/backend/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'utilsadm';
-$db['default']['password'] = 'utilsadm';
+$db['default']['password'] = 'Welcome.2.Mysql';
 $db['default']['database'] = 'utilsdb';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
